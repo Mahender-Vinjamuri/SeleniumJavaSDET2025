8c4e542e31c552904777e0ee68b4972bbdd92e54
Madhuri <mchitturi2608@gmail.com>
2025-01-28 21:41:19 +0530

    Added pom.xml file

---
+++ src/test/java/com/JavaBasics/DataVariables.java
@@ -0,0 +1,26 @@
+package com.JavaBasics;
+
+public class DataVariables {
+	
+	int a = 10;     //instance variable - declare inside the class but outside the method
+	char c = 'A';
+	static String str = "Mahender";  //static variable
+	public int sum(int a, int b)
+	{
+	return a + b;
+	}
+
+	public static void main(String[] args) {
+		
+		int a = 50;   //local variable which declare inside the method
+		DataVariables obj = new DataVariables();
+		System.out.println(obj.a);
+		System.out.println(obj.c);
+		System.out.println(str);
+		System.out.println(obj.sum(10, 20));
+		System.out.println(a);
+		
+
+	}
+
+}
